cf41454feeaaaaea135a5b495c6f0b8dd6ead1e8
Brooke Heathcock <AtomicGardenInteractive@Gmail.com>
2022-01-07 15:13:49 +0000

    Finished Main Menu
    Added Resolution and Quality Settings

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public TMPro.TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResoltionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+             string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + " Hz";
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+                
+            {
+                currentResoltionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResoltionIndex;
+        resolutionDropdown.RefreshShownValue();
+        
+    }
+
+    public void SetScreenResolution(int resoltuionIndex)
+    {
+        Resolution resolution = resolutions[resoltuionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+        Debug.Log(volume);
+    }
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Debug.Log("Fullscreen Toggled");
+        Screen.fullScreen = isFullScreen;
+    }
+}
